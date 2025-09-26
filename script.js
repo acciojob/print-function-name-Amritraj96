8ab6ae249852adae959e2d99cb217a7a03235f3a
Amritraj96 <amritraj.up.yadav@gmail.com>
2025-09-26 23:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// script.js
+
+function functionName() {
+  // using arguments.callee.name to print the function's own name
+  alert(arguments.callee.name);
+}
+
+// Example: calling the function
+functionName(); // will alert "functionName"
